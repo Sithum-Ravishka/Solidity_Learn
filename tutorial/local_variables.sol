0f77735f391ab9d54492ccb1a56394fce8d0d3fd
Sithum-Ravishka <sithumravishka1@gmail.com>
2023-11-17 10:17:25 +0530

    local variables

--- tutorial/local_variables.sol
+++ tutorial/local_variables.sol
@@ -1,3 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.22;
 
+contract LocalVariables {
+    uint public i;
+    bool public b;
+    address public myAddress;
+
+    function foo() external {
+        uint x = 200;
+        bool y = false;
+
+        x += 20;
+        y = true;
+
+        i = 300;
+        b = true;
+        myAddress = address(1554233);
+    }
+}
