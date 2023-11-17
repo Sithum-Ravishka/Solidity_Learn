1bd77a30d3f3fbd13a238fe1142590d9ec84d2a1
Sithum-Ravishka <sithumravishka1@gmail.com>
2023-11-17 17:12:08 +0530

    default value

---
+++ tutorial/default_value.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+contract DefaultValue {
+    // Satte and local variable have default value, if you not assign value to variable than get default value.
+
+    
+}
