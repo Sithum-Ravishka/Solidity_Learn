e7781c18378fabf31e6f85b99aaca956ad48e332
Sithum-Ravishka <sithumravishka1@gmail.com>
2024-02-14 12:04:52 +0530

    error handle using assert

--- tutorial/require.sol
+++ tutorial/require.sol
@@ -11,7 +11,7 @@ contract ErrorHandling{
     }
 
     function addBalance(uint _add) public returns (uint) {
-        require(msg.sender == owner); // now only owner can add amount to address
+        require(msg.sender == owner, "Only owner can add money to account"); // now only owner can add amount to address
         balance[msg.sender] += _add;
         return balance[msg.sender];
     }
@@ -21,9 +21,17 @@ contract ErrorHandling{
     }
 
     function transfer (address recipient, uint amount) public {
-        require(balance[msg.sender] >= amount);  // amount need lees than sender account ammount
-        require(msg.sender != recipient);        // check address is not sender address
+        require(balance[msg.sender] >= amount, "Balance not sufficient");  // amount need lees than sender account ammount
+        require(msg.sender != recipient, "Don't transfer money to yourself");  // check address is not sender address
+
+        uint previousSenderBalance = balance[msg.sender];
+
         _transfer(msg.sender, recipient, amount);
+
+        assert(balance[msg.sender] == previousSenderBalance - amount); // If i have 1000 after i send 500 and need true this
+        // assert check balance[msg.sender] and previousSenderBalance - amount are 500 | this always true
+
+
     }
 
     function _transfer(address from, address to, uint amount) private {
